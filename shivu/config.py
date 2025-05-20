cf6ef8b446110dafd33490bed57b1f6dc6f59bc3
Nexxababy <kartik18210@gmail.com>
2025-05-20 16:04:17 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -2,9 +2,9 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    OWNER_ID = "7011990425"
+    OWNER_ID = "7639271205"
     sudo_users = ["6890857225", "7011990425", "6264760837", "5527022252", "6384484443", "1850686769"]
-    GROUP_ID = "-1002041586214"
+    GROUP_ID = "-1002097449198"
     TOKEN = "your bot token"
     mongo_url = "your db"
     PHOTO_URL = ["https://telegra.ph/file/7e5398823512d307128a3.jpg", "https://telegra.ph/file/c45dcb207d81e97cb4f6a.jpg", "https://telegra.ph/file/0bc6d65878e8300fbf0f8.jpg", "https://telegra.ph/file/0afb45203ff162ee7227b.jpg"]
