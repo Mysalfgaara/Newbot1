666074d476bbf0f95b41fe4b4b6207ae5442e926
Mysalfgaara <pabitrabarmanaaa089@gmail.com>
2025-05-28 16:57:30 +0530

    Update fbo.py

--- shivu/modules/fbo.py
+++ shivu/modules/fbo.py
@@ -8,7 +8,7 @@ from shivu import application, user_collection
 from shivu import shivuu as app
 
 MUST_JOIN = "DemonSlayer_Update"  # Replace with your group/channel username or ID
-allowed_group_id = -1002041586214  # Replace with your allowed group ID
+allowed_group_id = -1002097449198  # Replace with your allowed group ID
 
 # Additional functionality for rewarding users who claim
 async def claim_reward(update: Update, context: CallbackContext):
